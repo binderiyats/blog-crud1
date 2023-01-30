bb9fa3e9d0e784889f78fcf6aab6aa77658401ee
Binderiya <binderiyatsogthers@gmail.com>
2023-01-30 18:48:31 +0800

    started category

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function Categories() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/categories`)
+      .then((res) => {
+        setCategories(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="container-sm body-container">
+        <Heading title="Categories" handleShow={showCreateModal} />
+        <CategoryList items={categories} onEdit={showEditModal} />
+      </div>
+      <DynamicModal
+        content={modalContent}
+        handleClose={modalClose}
+        show={modalShow}
+        title="Create category"
+      />
+    </div>
+  );
+}
